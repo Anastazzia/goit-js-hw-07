a6245ccbfe5fbebbc2bf9831f35f9ca3aa6769d7
Anastazzia <malashevychanna@gmail.com>
2023-10-15 21:42:26 +0300

    1 TASK
    1/2/3 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const imgContainer = document.querySelector('.gallery');
+const imgMarkup = createItemsCards(galleryItems);
+
+imgContainer.insertAdjacentHTML('beforeend', imgMarkup);
+
+imgContainer.addEventListener('click', (event) => {
+    if (event.target.classList.contains('gallery__image')) {
+        event.preventDefault();
+
+    }
+});
+
+function createItemsCards(imgCards) {
+    return imgCards.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    }).join('');
+
+};
+
+imgContainer.addEventListener('click', (event) => {
+    if (event.target.tagName === 'IMG') {
+        const largeImgUrl = event.target.getAttribute('data-source')
+        console.log('URL: ', largeImgUrl)
+    };
+});
