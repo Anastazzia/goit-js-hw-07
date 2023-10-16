56a0f8a1f0fde8355484ee859105d9c6bcc321a1
Anastazzia <malashevychanna@gmail.com>
2023-10-16 14:25:30 +0300

    2 Task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery')
+const imgMarkup = createImgCards(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', imgMarkup);
+
+const imgLiLinks = document.querySelectorAll('.gallery__item');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    /* options */
+    captions: true,
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
+
+function createImgCards(imgCards) {
+    return imgCards.map(({
+        preview, original, description
+    }) => {
+        return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+
+    }).join('');
+};
+
